699a69e236ef35f99c2c207b2582da25df92edd4
Sammy_K <samkristoff@gmail.com>
2014-01-05 10:38:07 -0600

    Update enable_serial.sh

--- scripts/setup/enable_serial.sh
+++ scripts/setup/enable_serial.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-echo -e "\n\nEnabling Serial Support\n"
+echo -e "\n\nEnabling Serial Support For User $USER\n"
 
+#Edit Config To Load Serial Driver On Boot
 
-#Enable Serial Port
-modprobe serial_core
-modprobe amba-pl011
+sudo bash -c "echo -e '\n#Serial Support' >> /etc/modules"
+sudo bash -c "echo 'serial_core' >> /etc/modules"
+sudo bash -c "echo 'amba-pl011' >> /etc/modules"
+
+#Add Current User To dialout Group So They Can Access The Serial Port
+sudo usermod -a -G dialout "$USER" 
 
 #Set Baud Rate
 sudo stty -F /dev/ttyAMA0 115200
@@ -12,4 +16,4 @@ sudo stty -F /dev/ttyAMA0 115200
 #Enable Raw Output
 sudo stty -F /dev/ttyAMA0 raw
 
-########NEED TO GIVE USER PERMISSION TO ACCESS SERIAL PORT!!!!!!!
+echo -e "Reboot Required For Changes To Take Effect\n"
